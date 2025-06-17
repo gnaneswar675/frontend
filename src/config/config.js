dba4d1fd94fcb01e291cd392bcbbc42b27fbadb7
Gnaneswar Thavva <thavvagnaneswar675@gmail.com>
2025-06-17 10:40:26 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,5 @@
 const config = {
   API_BASE_URL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
-  UPLOAD_PATH: process.env.REACT_APP_UPLOAD_PATH || 'http://localhost:5000/uploads',
 };
 
 export default config;
